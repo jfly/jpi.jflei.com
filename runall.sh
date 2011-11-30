efe79685a943356340c6cc700da88e70888c9728
Jeremy Fleischman <jeremyfleischman@gmail.com>
2011-11-30 00:18:18 -0800

    runall now checks out repos, rather than relying upon git submodules. git submodules don't behave exactly like fresh repos (can't immediately do a git pull). I also don't want to have to commit to jflei.com when the sub servers update.

--- runall.sh
+++ runall.sh
@@ -3,19 +3,27 @@
 # TODO - now that i think about it, perhaps most of this is best inside of the
 # appropriate repos...
 
+if [ -d "servers" ]; then
+	echo "servers directory already exists, not checking out sub servers"
+else
+	mkdir servers
+	git clone --recursive git://github.com/jfly/tnoodle.git servers/tnoodle
+	git clone --recursive git://github.com/jfly/Hackathon2011.git servers/Hackathon2011
+fi
+
 if [ "`screen -ls | grep tnoodle`" != "" ]; then
 	echo "Screen session tnoodle already running, killing it"
 	# TODO if there's more than one session named tnoodle, this doesn't work
 	screen -S tnoodle -X quit
 fi
-( cd deps/tnoodle; screen -d -m -S tnoodle -c ../../tnoodlescreenrc )
+( cd servers/tnoodle; screen -d -m -S tnoodle -c ../../tnoodlescreenrc )
 
 if [ "`screen -ls | grep battle`" != "" ]; then
 	echo "Screen session battle already running, killing it"
 	# TODO if there's more than one session named battle, this doesn't work
 	screen -S battle -X quit
 fi
-( cd deps/Hackathon2011; screen -d -m -S battle -c ../../battlescreenrc )
+( cd servers/Hackathon2011; screen -d -m -S battle -c ../../battlescreenrc )
 
 # nginx is daemonized, so there's no use to running it inside of a screen
 sudo killall nginx
