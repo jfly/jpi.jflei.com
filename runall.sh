69d758c488c51274a86d9d6c57c3e36b1029bbf6
Jeremy Fleischman <jeremyfleischman@gmail.com>
2013-03-23 19:00:16 -0700

    Updated runall script to use new git-tools infrastructure.

--- runall.sh
+++ runall.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# TODO - now that i think about it, perhaps most of this is best inside of the
-# appropriate repos...
-
 # From http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "Changing to $DIR"
@@ -13,23 +10,11 @@ if [ -d "servers" ]; then
 else
 	mkdir servers
 	git clone --recursive git://github.com/jfly/tnoodle.git servers/tnoodle
-	git clone --recursive git://github.com/jfly/tnoodle.git servers/tnoodle-battle
 fi
 
-if [ "`screen -ls | grep tnoodle`" != "" ]; then
-	echo "Screen session tnoodle already running, killing it"
-	# TODO if there's more than one session named tnoodle, this doesn't work
-	screen -S tnoodle -X quit
-fi
-( cd servers/tnoodle; screen -d -m -S tnoodle -c ../../tnoodlescreenrc )
+servers/tnoodle/RunAll.py --inject `readlink -e tnoodle-analytics.js`
 
-if [ "`screen -ls | grep battle`" != "" ]; then
-	echo "Screen session battle already running, killing it"
-	# TODO if there's more than one session named battle, this doesn't work
-	screen -S battle -X quit
-fi
-( cd servers/tnoodle-battle; screen -d -m -S battle -c ../../battlescreenrc )
-
+# TODO port jflei code to use git-tools stuff <<<
 if [ "`screen -ls | grep jflei`" != "" ]; then
 	echo "Screen session jflei already running, killing it"
 	# TODO if there's more than one session named jflei, this doesn't work
